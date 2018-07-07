390d2b93ba96b8ae56b2e306fea18e1597544951
stanislaff <slep0y@yandex.ru>
2018-07-07 19:30:34 +0300

    Push lesson2

--- src/App.js
+++ src/App.js
@@ -9,13 +9,45 @@ import Header from './components/Header';
 import Menu from './components/Menu';
 import Main from './components/Main';
 
+const url = process.env.PUBLIC_URL+'/img/bg.png';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding: 20px;
+`
+
+const MainWrapper = styled.div`
+  padding-top: 130px;
+  height: 600px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Header />
-        <Menu />
-        <Main />
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,55 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+const Repair = styled.div`
+    color: #ffffff;
+    font-weight: 700;
+    line-height: 50px;
+    font-size: 40px;
+
+    span{
+        font-size: 24px;
+        display: block;
+        line-height: 30px;
+    }
+`
+const LightText = styled.div`
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 28px;
+    margin-top: 54px;
+`
+
+const CallBtn = styled.button`
+    width: 247px;
+    height: 67px;
+    background-color: #ffa14b;
+    color: #fff;
+    border-radius: 400px;
+    border: none;
+    margin-top: 32px;
+`
+
 class Main extends React.Component {
     render(){
         return (
-            <h1> Main </h1>
+            <Row>
+                <Col lg={5}>
+                    <Repair>
+                        Качественный ремонт
+                        <span>iphone за 35 минут и гарантия 1 год</span>
+                    </Repair>
+                    <LightText>Оставьте заявку на бесплатную диагностику без очереди,
+                        и получите защитное стекло, стоимостью 1000 рублей,
+                        с установкой в подарок!
+                    </LightText>
+                    <CallBtn>Отправить заявку!</CallBtn>
+                </Col>
+                <Col lg={6} lgOffset={1}>
+                    
+                </Col>
+            </Row>
         )
     }
 }
